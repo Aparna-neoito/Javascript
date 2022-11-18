543aac937011512dfcf4e69e6e8c8f3b2a208243
APARNAB <aparna.b@neoito.com>
2022-11-18 16:59:45 +0530

    task 12:comparison of expressions

---
+++ 6.Comparisons/task12.js
@@ -0,0 +1,14 @@
+let a=5 > 4;
+console.log(a)
+let b="apple" > "pineapple";
+console.log(b);
+let c="2" > "12"; //doubt
+console.log(c);
+let d=undefined == null;
+console.log("d="+d);
+let e=undefined === null;
+console.log(e)
+let f=null == "\n0\n";
+console.log(typeof(+"\n0\n"));
+let g=null === +"\n0\n";
+console.log(g);
