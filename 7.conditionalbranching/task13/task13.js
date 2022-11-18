9e74619ad2c164b636b23906065e04eeef91bcd2
APARNAB <aparna.b@neoito.com>
2022-11-18 17:38:52 +0530

    task 13,14:if if else

---
+++ 7.conditionalbranching/task13/task13.js
@@ -0,0 +1,6 @@
+let a=prompt("enter a number");
+if(a==0){
+      alert('hello');
+}else{
+      alert('hi');
+}
---
+++ 7.conditionalbranching/task14/task14.js
@@ -0,0 +1,7 @@
+let checkName=prompt("What is the “official” name of JavaScript?");
+let jsName="ECMAScript";
+if(checkName==jsName){
+     alert("true")
+}else{
+      alert("You don’t know? ECMAScript!");
+}
