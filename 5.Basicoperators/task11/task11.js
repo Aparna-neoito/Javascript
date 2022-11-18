78edc842d728fff53311e64b61c703056d6b6721
APARNAB <aparna.b@neoito.com>
2022-11-18 14:53:57 +0530

    task 11:fix the addition

---
+++ 5.Basicoperators/task11/task11.js
@@ -0,0 +1,9 @@
+let a=prompt("first number?",1);
+let b=prompt("second number?",2);
+let sum=a+b;
+console.log(sum);
+
+let c=parseInt(prompt("first number?",1));
+let d=parseInt(prompt("second number?",2));
+let s=c+d;
+console.log(s);
