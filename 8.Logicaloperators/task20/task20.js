c89952dc5e2cca2bb6434d6b8e25604519da8e9b
APARNAB <aparna.b@neoito.com>
2022-11-21 12:54:10 +0530

    task 20:not logicaloperator

---
+++ 8.Logicaloperators/task20/task20.js
@@ -0,0 +1,8 @@
+// let age=parseInt(prompt('enter your age'));
+// let ans=(!(age>=14&&age<=90))?1:2;
+// alert(ans);
+
+
+let age2=parseInt(prompt('enter your age'));
+let rslt=(age2>=14||age2<=90)?1:2;
+alert(rslt);
