55a9f63baf4856689d33870b7f9a8fc4661dd5b1
APARNAB <aparna.b@neoito.com>
2022-11-21 16:07:40 +0530

    task 23,24:while and for loop

--- 8.Logicaloperators/task22/task22.js
+++ 8.Logicaloperators/task22/task22.js
@@ -1,15 +1,15 @@
 let person=prompt("who's there?");
 let password;
-if(person=='Admin'){
+if(person==='Admin'){
       password=prompt('Password?')
-      if(password=='TheMaster'){
+      if(password==='TheMaster'){
             alert('Welcome!')
-      }else if(password==""){
+      }else if(password===""||password===null){
             alert('Canceled')
       }else{
             alert('Wrong password')
       }
-}else if(person==""){
+}else if(person===""||person===null){
       alert("Canceled")
 }else{
       alert('I dont know you');
---
+++ 9.Loops: while and for/task24/task24.js
@@ -0,0 +1,5 @@
+let i=0;
+while(++i <5) alert(i);
+
+// let intial=0;
+// while(intial++ <5) alert(intial);
