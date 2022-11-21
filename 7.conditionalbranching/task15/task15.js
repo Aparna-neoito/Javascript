7ec08acf5797ef268a3f95e985f16f25915952f1
APARNAB <aparna.b@neoito.com>
2022-11-21 10:48:21 +0530

    task 16:if ternary operators

--- 7.conditionalbranching/task15/task15.js
+++ 7.conditionalbranching/task15/task15.js
@@ -1,4 +1,4 @@
-let number=prompt("enter a number");
+let number=parseInt(prompt("enter a number"));
 if(number>0){
       alert('1');
 }else if(number<0){
---
+++ 7.conditionalbranching/task16/task16.js
@@ -0,0 +1,5 @@
+let a,b;
+a=parseInt(prompt('enter first number'));
+b=parseInt(prompt('enter the second number'));
+let result=((a+b)<4)?'below':'Over';
+console.log(result);
