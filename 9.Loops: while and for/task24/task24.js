27133932759511158a592ff54c82f1e0425fd032
APARNAB <aparna.b@neoito.com>
2022-11-21 16:49:28 +0530

    task 25:for to while

--- 9.Loops: while and for/task24/task24.js
+++ 9.Loops: while and for/task24/task24.js
@@ -1,5 +1,14 @@
-let i=0;
-while(++i <5) alert(i);
+// let i=0;
+// while(++i <5) alert(i);
 
-// let intial=0;
+let intial=0;
 // while(intial++ <5) alert(intial);
+const a=++intial;
+
+// const a1=intial++;
+
+
+
+
+console.log(a)
+// console.log(a1)
