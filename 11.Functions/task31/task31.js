9f3d96d66bf4fe5e01a71d70a4680f217d57441e
APARNAB <aparna.b@neoito.com>
2022-11-22 14:17:42 +0530

    task31 :ternary and or operator

---
+++ 11.Functions/task31/task31.js
@@ -0,0 +1,9 @@
+function checkAge(age){
+      (age>8)? true : confirm('Did parents allow you');
+}
+checkAge(20);
+
+function checkAge2(age){
+    return (age>18) || confirm('Did parents allow you');
+}
+checkAge2(12);
